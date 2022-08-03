9248167893333752696a99640640e4a5ec774c69
Hinoki <hinokicrum@email.arizona.edu>
2022-08-03 15:42:52 -0700

    week 5

--- tests/database_test.py
+++ tests/database_test.py
@@ -1,5 +1,5 @@
 from db.mysql_repo import *
-import pandas as pd
+from app.services import *
 
 def test_database_tfidf():
     '''
@@ -28,18 +28,36 @@ def test_database_tfidf():
     db = Database(words, tfidfs, jp, eng, pos)
     db.create_database("korean_tfidf", "word", 'tfidf', 'japanese', 'english', 'pos')
     result = db.retrive_data("korean_tfidf")
-    '''
+
     df = pd.read_csv('df.csv', index_col=0)
 
-    tfidfs = df['tfidf'].tolist()
-    words = df.index.values.tolist()
+    #tfidfs = df['tfidf'].tolist()
+    #words = df.index.values.tolist()
     pos = df['POS'].tolist()
     eng = df['English'].tolist()
     jp = df['Japanese'].tolist()
-    repo = MysqlRepository()
-    #repo.del_table
+    '''
+
+    text = open('web_data.txt', 'r').read()
+    tk = TokenizeKoreanSent(text)
+    tokenized = tk.tokenize_korean()
+    df = VocabTFIDF(tokenized).df
+    tfidfs = df['tfidf'].tolist()[:3]
+    words = df.index.values.tolist()[:3]
+
+    tags = [str([y for x, y in TaggedSentence(word, 'korean').tagged]) for word in words]
+    j_lst = [TaggedSentence(word, 'korean').translate("ja") for word in words]
+    e_lst = [TaggedSentence(word, 'korean').translate("en") for word in words]
+
     #repo = MysqlRepository()
-    repo.insert_table(words, tfidfs, jp, eng, pos)
+    #repo.del_table()
+    repo = MysqlRepository()
+    repo.insert_table(words, tfidfs, j_lst, e_lst, tags)
     result = repo.load_lexicon()
-    assert result[0]['tfidf'] == 0.358313
+    for i in range(len(result)):
+        if result[i]['word'] == '고려':
+            print(result[i])
+        else:
+            continue
+    assert result[0] == {'word': '고려', 'tfidf': 0.358313, 'japanese': '考慮', 'english': 'Consideration', 'pos': "['Noun']"}
 
