f2bb642e853e1536e554e0f08a9136b7bfe515b5
Hinoki <hinokicrum@email.arizona.edu>
2022-08-14 13:08:52 -0700

    final for week 6

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,6 +1,6 @@
 import pytest
 import json
-from app.app1 import *
+from app1 import *
 
 
 @pytest.fixture
@@ -33,17 +33,4 @@ def test_post_data(client):
 
 
 
-'''
-import requests
 
-BASE = "http://127.0.0.1:5000/"
-
-def test_get_api():
-    response = requests.get(BASE + "word/天守")
-    print(response.json())
-    assert response.json() == {'english': 'Castle tower',
-                                'korean': '성 타워',
-                                'pos': "['Noun']",
-                                'tfidf': 0.491773,
-                                'word': '天守'}
-'''
