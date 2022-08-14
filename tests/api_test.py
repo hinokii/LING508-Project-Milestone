51df3bfc9ccdbfa5967903e52b5bdedaec6c93ac
Hinoki <hinokicrum@email.arizona.edu>
2022-08-14 11:44:37 -0700

    test

--- tests/api_test.py
+++ tests/api_test.py
@@ -18,7 +18,15 @@ def test_get_data(client):
     data = json.loads(rv.data)
     assert data[0].get('word') == "고려"
 
-
+def test_post_data(client):
+    rv = client.post('/post_data',
+                     data=json.dumps({"word": "의견", "japanese": "意見", "english": "Jones", "pos": "['Noun']"}),
+                     content_type='application/json')
+    response = json.loads(rv.data)
+    assert response.get("msg") == "success"
+    rv = client.get('/get_data')
+    data = json.loads(rv.data)
+    assert data[-1].get("japanese") == "意見"
 
 
 
