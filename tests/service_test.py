e1f943bd31b53ed6469462923d2de20639935a57
Hinoki <hinokicrum@email.arizona.edu>
2022-08-23 10:11:40 -0700

    update

--- tests/service_test.py
+++ tests/service_test.py
@@ -2,10 +2,10 @@ from app.services import *
 
 def test_service_korean():
     result = Services().show_result("단일화", "korean")
-    assert result.tfidf == 0.178815
-    assert result.japanese == '統一'
-    assert result.english == 'Unification'
-    assert result.pos == "['Modifier', 'Noun']"
+    assert result.tfidf == 0.297862
+    assert result.japanese == 'ハウジング'
+    assert result.english == 'Housing'
+    assert result.pos == "['Noun']"
 
 def test_service_japanese():
     result = Services().show_result('天守', 'japanese')
